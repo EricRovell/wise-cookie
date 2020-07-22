f77768ac713ed419a8197484bfef49c189be3d58
Eric Rovell <ericrovell@gmail.com>
2020-07-22 12:20:16 +0300

    delete odd files

--- src/util/randColor/randCMYK.js
+++
@@ -1,9 +0,0 @@
-import randFloat from "@util/math/randFloat.js";
-
-/**
- * Generates a random CMYK value color.
- * @returns {Number[]} CMYK values array
- */
-export default function randHSL() {
-  return [ randFloat(2), randFloat(2), randFloat(2), randFloat(2) ];
-}
