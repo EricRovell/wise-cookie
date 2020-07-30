cf7569f7ea0257bfd89efc9c77f8177abbc8bd35
Eric Rovell <ericrovell@gmail.com>
2020-07-30 15:00:15 +0300

    Layout Main structured

--- src/layout/LayoutMain.svelte
+++ src/layout/LayoutMain.svelte
@@ -3,6 +3,13 @@
   import Footer from "@src/layout/Footer.svelte";
 </script>
 
+<!--
+  @component
+  Main Layout component.
+  Contains the persistent components across all application:
+    - Header,
+    - Footer.
+-->
 <main id="layout-main">
   <Header />
   <div class="content">
@@ -13,14 +20,20 @@
 
 <style>
   #layout-main {
+    display: grid;
+    place-items: center;
     width: 100%;
   }
 
   .content {
     display: grid;
-    justify-items: center;
+
     width: 100%;
+    max-width: var(--max-width);
     min-height: 100vh;
+
+    margin-bottom: 15vh;
+    padding-top: 1.5em;
   }
 
   @media screen and (max-width: 720px) {
