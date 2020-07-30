9f6641bb83ea355677ef219fd0c08f48513488d3
Eric Rovell <ericrovell@gmail.com>
2020-07-30 14:59:10 +0300

    Home route

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,3 +1,27 @@
-<p>
-  Hello
-</p>
+<script>
+  import MrCookie from "@components/MrCookie.svelte";
+  import PhraseAwait from "@components/PhraseAwait.svelte";
+</script>
+
+<div class="container">
+  <PhraseAwait />
+  <MrCookie />  
+</div>
+
+<style>
+  .container {
+    display: grid;
+    grid-template: 1fr / 1fr 250px;
+    place-items: start center;
+    column-gap: 2.25em;
+  }
+
+  @media screen and (max-width: 720px) {
+    .container {
+      display: grid;
+      grid-template: min-content 1fr / 1fr;
+      place-items: start center;
+      column-gap: 2.25em;
+    }
+  }
+</style>
