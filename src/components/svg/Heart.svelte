fb5ba2dca383cc36c695ccd11a2df8769129b66b
Eric Rovell <ericrovell@gmail.com>
2020-07-30 15:03:07 +0300

    Heart svg component

---
+++ src/components/svg/Heart.svelte
@@ -0,0 +1,49 @@
+<!--
+  @component
+  Beating heart svg element.
+
+  Component has no logic or behaviour except the infinite animation.
+  Only for presentational purposes.
+-->
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+  <title>Love</title>
+  <path
+    d="M72.8,9.07A27.33,27.33,0,0,1,100,36.27c0,27.46-50,54.66-50,54.66S0,63.33,0,36.27A27.2,27.2,0,0,1,27.2,9.07h0A26.94,26.94,0,0,1,50,21.33,27.19,27.19,0,0,1,72.8,9.07Z" />
+</svg>
+
+<style>
+  svg {
+    animation: heartbeat 0.5s ease-in infinite alternate;
+  }
+
+  @keyframes heartbeat {
+    0% {
+      transform: scale(0.8);
+      fill: #d7443e;
+    }
+
+    5% {
+      transform: scale(0.9);
+      fill: #d7443e;
+    }
+
+    10% {
+      transform: scale(0.8);
+    }
+
+    15% {
+      transform: scale(1.1);
+      fill: #d7443e;
+    }
+
+    50% {
+      transform: scale(0.8);
+      fill: red;
+    }
+
+    100% {
+      transform: scale(0.8);
+      fill: red;
+    }
+  }
+</style>
