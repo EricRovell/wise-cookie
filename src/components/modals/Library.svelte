d8b26c604fbd89015be147b7d5eb05f5f33fc304
Eric Rovell <ericrovell@gmail.com>
2020-07-30 15:03:45 +0300

    Library status modal message component

---
+++ src/components/modals/Library.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { onMount } from "svelte";
+  import App from "@stores/app.js";
+
+  export let hide;
+  export let cookies = $App.PHRASES;
+  let eatenCookies = 0;
+
+  onMount(async () => {
+    eatenCookies = await $App.db.getCookiesEaten();
+  });
+</script>
+
+<div>
+  <p>We have {cookies - eatenCookies} unique cookies of wisdom awaiting for you!</p>
+  <button on:click={hide}>ok</button>
+</div>
