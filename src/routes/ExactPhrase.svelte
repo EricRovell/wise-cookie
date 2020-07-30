d2c4aeb763a5361e6757efdf7a4f333f53a20865
Eric Rovell <ericrovell@gmail.com>
2020-07-30 14:59:33 +0300

    Exact Phrase URL page

---
+++ src/routes/ExactPhrase.svelte
@@ -0,0 +1,19 @@
+<script>
+  import App from "@stores/app.js"; 
+
+  import Loader from "@components/Loader.svelte";
+  import Phrase from "@components/Phrase.svelte";
+
+  // category, #pack, #position
+  export let params = {};
+</script>
+
+<div>
+  {#await $App.phrase.fromURL(params)}
+    <Loader />
+  {:then phrase}
+    <Phrase {phrase} />
+  {:catch error}
+    <p>Something is wrong: {error}</p>
+  {/await}
+</div>
