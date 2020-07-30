812fd6aab42a9300e9386227d8e20618ec083418
Eric Rovell <ericrovell@gmail.com>
2020-07-30 14:56:04 +0300

    App component

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,16 @@
   import routes from "@routes/routes.js";
 
   import LayoutMain from "@src/layout/LayoutMain.svelte";
+  import Modal from "@components/Modal.svelte";
 </script>
+
+<svelte:head>
+  <title>Wise Cookie</title>
+</svelte:head>
 
 <body>
   <LayoutMain>
     <Router {routes} />
   </LayoutMain>
+  <Modal />
 </body>
