80ec6a63ff74a7efe7c8891f7d5e666b26d7da5b
Eric Rovell <ericrovell@gmail.com>
2020-07-30 15:02:03 +0300

    Loader Component

---
+++ src/components/Loader.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { fade } from "svelte/transition";
+</script>
+
+<div transition:fade>
+  <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 27">
+    <title>Untitled-1</title>
+    <path d="M98,2V25H2V2H98m2-2H0V27H100V0Z"/>
+    <text
+      x="50%"
+      y="50%"
+      dominant-baseline="middle"
+      text-anchor="middle"
+      style="font-size: 10px;font-family: MyriadPro-Regular, Myriad Pro">
+        Loading...
+    </text>
+    <path d="M98,2V25H2V2H98m2-2H0V27H100V0Z"/>
+  </svg>
+</div>
+
+<style>
+  div {
+    width: 100%;
+  }
+
+  svg {
+    width: 120px;
+    height: 50px;
+  }
+
+  text {
+    color: var(--color-3);
+  }
+</style>
