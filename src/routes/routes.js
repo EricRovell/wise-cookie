c81b2c46106c8dbfba58d7f7483a5e47e79dc98b
Eric Rovell <ericrovell@gmail.com>
2020-08-02 12:18:54 +0300

    using page404 in route map

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,6 +5,7 @@ import ExactPhrase from "./ExactPhrase.svelte";
 import Help from "@routes/Help.svx";
 import About from "@routes/About.svx";
 import Favourites from "@routes/Favourites.svelte";
+import Page404 from "@routes/404.svelte";
 
 export default {
   // exact
@@ -12,5 +13,6 @@ export default {
   "/phrase/:category/:pack/:position": ExactPhrase,
   "/help": Help,
   "/about": About,
-  "/favourites": Favourites
+  "/favourites": Favourites,
+  "*": Page404
 };
