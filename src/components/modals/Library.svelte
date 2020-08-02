9616f4db4e1bba67bca75c4586fc695adb48ce6c
Eric Rovell <ericrovell@gmail.com>
2020-08-02 12:19:43 +0300

    updated style for modal subcomponent

--- src/components/modals/Library.svelte
+++ src/components/modals/Library.svelte
@@ -1,6 +1,8 @@
 <script>
   import { onMount } from "svelte";
   import App from "@stores/app.js";
+  import Button from "@components/Button.svelte";
+  import Cookie from "@components/svg/Cookie.svelte";
 
   export let hide;
   export let cookies = $App.PHRASES;
@@ -12,6 +14,21 @@
 </script>
 
 <div>
-  <p>We have {cookies - eatenCookies} unique cookies of wisdom awaiting for you!</p>
-  <button on:click={hide}>ok</button>
+  <p>There are {cookies - eatenCookies} unique cookies awaits for you in our library!</p>
+  <Cookie size="125px" eaten={true} />
+  <Button message="OK" handleClick={() => hide()} />
 </div>
+
+<style>
+  div {
+    display: grid;
+    place-items: center;
+    grid-auto-rows: min-content;
+    row-gap: 1.25em;
+  }
+
+  p {
+    text-align: center;
+    font-size: 1.4em;
+  }
+</style>
