ae5f25d29c933619992ee7fb5dbdef4f2df7b828
Eric Rovell <ericrovell@gmail.com>
2020-08-02 13:29:18 +0300

    updated structure

--- src/routes/404.svelte
+++ src/routes/404.svelte
@@ -1,23 +1,36 @@
 <script>
-  import MrCookie from "@components/MrCookieShocked.svelte";
+  import MrCookie from "@components/MrCookie.svelte";
 </script>
 
 <svelte:head>
   <title>404: Sorry, but there no cookies!</title>
 </svelte:head>
 
-<div>
-  <MrCookie />
+<div class="container">
+  <div>
+    <MrCookie type="shocked" />
+  </div>
   <p>404</p>
   <p>Sorry, there are no cookies on this page!</p>
 </div>
 
 <style>
-  div {
+  .container {
+    --img-size: 275px;
+
+    position: sticky;
+    top: 10px;
+    height: min-content;
+
     display: grid;
     grid-auto-rows: min-content;
     row-gap: 1.5em;
     place-items: start center;
+  }
+
+  .container > div {
+    width: var(--img-size);
+    height: var(--img-size);
   }
 
   p:first-of-type {
@@ -27,5 +40,12 @@
 
   p:last-of-type {
     font-size: 1.75em;
+    text-align: center;
+  }
+
+  @media screen and (max-width: 720px) {
+    .container {
+      --img-size: 50vw;
+    }
   }
 </style>
