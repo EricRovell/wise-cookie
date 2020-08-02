d83b54e6d9b6e0d04391b8d4590c24211b39d93d
Eric Rovell <ericrovell@gmail.com>
2020-08-02 12:22:55 +0300

    Simplified getting promise wrapper

---
+++ src/components/PhrasePromise.svelte
@@ -0,0 +1,21 @@
+<script>
+  import Phrase from "@components/Phrase.svelte";
+  import PhraseEmpty from "@components/PhraseEmpty.svelte";
+  import Loader from "@components/Loader.svelte";
+
+  export let promisedPhrase;
+</script>
+
+<div>
+  {#await promisedPhrase}
+    <Loader />
+  {:then phrase}
+    {#if phrase}
+      <Phrase {phrase} />
+    {:else}
+      <PhraseEmpty />
+    {/if}
+  {:catch error}
+    <p>Something is wrong: {error.message}</p>
+  {/await}
+</div>
