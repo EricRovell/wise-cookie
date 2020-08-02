795950815648bc1acedef6c23377e7f98ea81fa0
Eric Rovell <ericrovell@gmail.com>
2020-08-02 12:19:14 +0300

    new source; font size update

--- src/layout/Footer.svelte
+++ src/layout/Footer.svelte
@@ -5,14 +5,22 @@
 <footer>
   <div class="container">
     <span>
-      Wise Cookie v.0.1
+      Wise Cookie v.0.5
     </span>
     <div class="author">
-      <span>Made with</span>
-      <Heart />
-      <a href="https://github.com/EricRovell">
-        by Eric Rovell
-      </a>
+      <div>
+        <span>🍪 design</span>
+        <a href="https://web.marcelforart.com/victoriakan">
+          by Victoria
+        </a>
+      </div>
+      <div>
+        <span>Made with</span>
+        <Heart />
+        <a href="https://github.com/EricRovell">
+          by Eric Rovell
+        </a>
+      </div>
     </div>
   </div>
 </footer>
@@ -36,21 +44,20 @@
     margin: auto;
   }
 
+  /* app name */
+  .container > span {
+    display: flex;
+    align-items: flex-end;
+  }
+
   .author {
     display: grid;
-    grid-auto-flow: column;
-    column-gap: 0.25em;
-    place-items: center;
+    row-gap: 0.3em;
+    place-items: center end;
   }
 
   .author a {
     color: var(--color-3);
-  }
-
-  @media screen and (max-width: 720px) {
-    footer {
-      font-size: 0.65em;
-    }
   }
 
 </style>
