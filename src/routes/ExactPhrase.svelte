1ea87c04fb0aa08eca5988119f8840289c6ceceb
Eric Rovell <ericrovell@gmail.com>
2020-08-02 12:24:29 +0300

    Updated head

--- src/routes/ExactPhrase.svelte
+++ src/routes/ExactPhrase.svelte
@@ -6,10 +6,33 @@
 
   // category, #pack, #position
   export let params = {};
+
+  let promise = $App.phrase.fromURL(params);
 </script>
 
+<svelte:head>
+  {#await promise}
+    <title>Phrase is loading...</title>
+  {:then phrase}
+    <title>Phrase from {phrase.author}</title>
+    <!-- og -->
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content="Wise Cookie">
+    <meta property="og:title" content="Wise Cookie">
+    <meta property="og:description" content={`Wise Cookie: phrase from ${phrase.author}`}>
+    <meta property="og:image" content="https://wise-cookie.com/images/cookie-clever.webp">
+    <meta property="og:url" content="https://wise-cookie.com">
+    <!--Twitter-->
+    <meta name="twitter:title" content="Wise Cookie">
+    <meta name="twitter:description" content="Wise Cookie: Get your daily piece of inspiration!">
+    <meta name="twitter:image" content="https://wise-cookie.com/images/cookie-clever.webp">
+    <meta name="twitter:card" content={`Wise Cookie: phrase from ${phrase.author}`}>
+    <meta name="twitter:image:alt" content="Image of really clever Cookie">
+  {/await}
+</svelte:head>
+
 <div>
-  {#await $App.phrase.fromURL(params)}
+  {#await promise}
     <Loader />
   {:then phrase}
     <Phrase {phrase} />
