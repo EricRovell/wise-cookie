ef360a45ecdff4442a4f4974dbfa1989dcac5a3c
Eric Rovell <ericrovell@gmail.com>
2020-08-02 12:24:46 +0300

    Using MrCookie; styles update

--- src/routes/Favourites.svelte
+++ src/routes/Favourites.svelte
@@ -3,7 +3,12 @@
 
   import FadingWrapper from "@src/layout/FadingWrapper.svelte";
   import Phrase from "@components/Phrase.svelte";
+  import MrCookie from "@components/MrCookie.svelte";
 </script>
+
+<svelte:head>
+  <title>Favouries: Your personal storage</title>
+</svelte:head>
 
 <FadingWrapper>
   <div class="container">
@@ -13,7 +18,10 @@
       {#each phrases as [ id, phrase ]}
         <Phrase phrase={new $App.phrase({ id, ...phrase })} />
       {:else}
-        <p>The list is empty.</p>
+        <div class="empty-list">
+          <MrCookie type="shocked" />
+          <p>Woah! The list is empty!</p>
+        </div>
       {/each}
     {:catch error}
       <p>Something is wrong: {error}</p>
@@ -26,5 +34,20 @@
     display: grid;
     grid-auto-rows: min-content;
     row-gap: 2.5em;
+  }
+
+  .empty-list {
+    display: grid;
+    place-items: center;
+    row-gap: 1em;
+
+    position: sticky;
+    top: 10px;
+    width: 275px;
+    height: 275px;
+  }
+
+  .empty-list p {
+    font-size: 1.5em;
   }
 </style>
