3f000855117cf5b20415a746785eb6bc99275e00
Eric Rovell <ericrovell@gmail.com>
2020-08-02 12:18:42 +0300

    page 404

---
+++ src/routes/404.svelte
@@ -0,0 +1,31 @@
+<script>
+  import MrCookie from "@components/MrCookieShocked.svelte";
+</script>
+
+<svelte:head>
+  <title>404: Sorry, but there no cookies!</title>
+</svelte:head>
+
+<div>
+  <MrCookie />
+  <p>404</p>
+  <p>Sorry, there are no cookies on this page!</p>
+</div>
+
+<style>
+  div {
+    display: grid;
+    grid-auto-rows: min-content;
+    row-gap: 1.5em;
+    place-items: start center;
+  }
+
+  p:first-of-type {
+    font-size: 3em;
+    font-weight: 700;
+  }
+
+  p:last-of-type {
+    font-size: 1.75em;
+  }
+</style>
