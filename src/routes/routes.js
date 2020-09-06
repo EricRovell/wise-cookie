0e1e46fc99c624f236a90aa4aeca5a9c9e94dbde
Eric Rovell <ericrovell@gmail.com>
2020-09-06 14:05:56 +0300

    updated phrase id path; new secret route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,15 +4,16 @@ import Home from "./Home.svelte";
 import ExactPhrase from "./ExactPhrase.svelte";
 import Help from "@routes/Help.svx";
 import About from "@routes/About.svx";
+import Secret from "@routes/Secret.svelte";
 import Favourites from "@routes/Favourites.svelte";
 import Page404 from "@routes/404.svelte";
 
 export default {
-  // exact
   "/": Home,
-  "/phrase/:category/:pack/:position": ExactPhrase,
+  "/phrase/:fragment/:position": ExactPhrase,
   "/help": Help,
   "/about": About,
   "/favourites": Favourites,
+  "/secret/:pass": Secret,
   "*": Page404
 };
