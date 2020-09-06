ab6e347507b5cfc144c5d56fc047a42d846e2138
Eric Rovell <ericrovell@gmail.com>
2020-09-06 14:09:14 +0300

    using types; updated props; glow state

--- src/components/Phrase/Phrase.svelte
+++ src/components/Phrase/Phrase.svelte
@@ -1,10 +1,13 @@
-<script>
+<script lang="ts">
   import { setContext } from "svelte";
-  import { fade } from "svelte/transition";
+  import { slide } from "svelte/transition";
 
-  import PhraseControls from "@components/PhraseControls.svelte";
+  import type { Phrase } from "#types";  
 
-  export let phrase;
+  import PhraseControls from "@components/Phrase/PhraseControls.svelte";
+
+  export let phrase: Phrase = null;
+  export let glowing: boolean = false;
 
   setContext("phrase", phrase);
 </script>
@@ -14,14 +17,14 @@
   Visual represention of the phrase object.
 
   Props:
-    - id {string}: the identificator of the phrase,
-    - author {string}: the author of the phrase,
-    - body {string}: the message of the phrase.
+    - id: the identificator of the phrase,
+    - author: the author of the phrase,
+    - body: the message of the phrase.
 
   Component is not reactive, that's why to pass the phrase data it sets
-  the context with "phrase" key. 
+  the context with "phrase" key.
 -->
-<article in:fade>
+<article transition:slide class:glowing>
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <path d="M77.42,17.89A22.89,22.89,0,0,0,54.57,41,23.24,23.24,0,0,0,77.83,64.13l.44,0A29.24,29.24,0,0,1,60,70.55a5.77,5.77,0,0,0-5.77,5.78c0,3.19,2.06,5.78,5.25,5.78C82.09,82.11,100,63.67,100,41v0C100,28.23,90.19,17.89,77.42,17.89Z" />
     <path d="M23.27,17.89A22.91,22.91,0,0,0,.4,41,23.23,23.23,0,0,0,23.65,64.13l.45,0A29.32,29.32,0,0,1,5.76,70.55,5.77,5.77,0,0,0,0,76.33c0,3.19,2.09,5.78,5.28,5.78A40.73,40.73,0,0,0,45.88,41v0C45.88,28.23,36,17.89,23.27,17.89Z" />
@@ -51,6 +54,8 @@
     padding: 2em 1.5em 1.5em 1.5em;
     border: 1px solid rgb(0 0 0 / 0.1);
     border-radius: 10px;
+
+    font-size: 1.25em;
   }
 
   svg {
@@ -66,10 +71,37 @@
   div {
     display: flex;
     justify-content: space-between;
+    align-items: flex-end;
   }
 
   .author {
     justify-self: end;
+    align-self: end;
     font-style: italic;
+    text-align: end;
+
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+
+    padding-left: 1em;
   }
+
+  /* glow */
+  .glowing {
+    animation: glowing 2s ease-in-out 0.1s 2 alternate;
+  }
+
+  @keyframes glowing {
+    0% {
+      filter: drop-shadow(0 0 0px var(--color-3));
+    }
+    70% {
+      filter: drop-shadow(0 0 5px var(--color-3));
+    }
+    100% {
+      filter: drop-shadow(0 0 0 var(--color-3));
+    }
+  }
+
 </style>
