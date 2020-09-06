145dd91de0a17a30bccb102e9197eac2b6343cb0
Eric Rovell <ericrovell@gmail.com>
2020-09-06 14:01:55 +0300

    simplified theme getter

--- src/app/User.ts
+++ src/app/User.ts
@@ -1,7 +1,7 @@
 import db from "./DB";
 import map from "@public/data/map";
 
-import type { Theme, Phrase, PhraseID, PhrasesFavourite, PhraseIDString, UniqueCookies } from "#types/types";
+import type { Theme, Phrase, PhraseID, PhrasesFavourite, PhraseIDString, UniqueCookies, PhraseWithTimestamp } from "#types";
 
 /**
  * Provides access to user preferences and saved data (locally)
@@ -37,15 +37,8 @@ export default class User {
   /**
    * Switches currently preffered theme and returns the new value.
    */
-  public static async changeTheme(): Promise<Theme> {
-    const prefferedTheme = await db.getTheme();
-
-    if (prefferedTheme === "light") {
-      await db.setTheme("dark");
-      return "dark";
-    }
-
-    return "light";
+  public static async changeTheme(theme: Theme): Promise<void> {
+    await db.setTheme(theme);
   }
 
   /* Current phrases */
@@ -53,7 +46,7 @@ export default class User {
   /**
    * Retrieves a list of current phrases the user has.
    */
-  public static async getCurrentPhrases(): Promise<Phrase[]> {
+  public static async getCurrentPhrases(): Promise<PhraseWithTimestamp[]> {
     const currentPhrases = await db.getCurrentPhrases();
     return [ ...currentPhrases.values() ];
   }
@@ -84,10 +77,8 @@ export default class User {
   /**
    * If phrase is not "favourite" -> marks as favourite, otherwise -> removes the status of favourite.
    */
-  public static async markFavouritePhrase(phrase: Phrase): Promise<void> {
+  public static async markFavouritePhrase(phrase: Phrase): Promise<"added" | "removed" | "error"> {
     try {
-      console.log(phrase);
-
       let favourites: PhrasesFavourite = await db.getFavouritePhrases();
       let idString: PhraseIDString = phrase.id.toString();
 
@@ -95,7 +86,7 @@ export default class User {
       // deletion is successful only when it was here and was deleted
       if (favourites.delete(idString)) {
         await db.setFavouritePhrases(favourites);
-        return;
+        return "removed";
       }
 
       // not marked as favourite:
@@ -103,9 +94,11 @@ export default class User {
       //  construct new Map from entries to set the order (LIFO)
       //  slice to limit the number of records
       await db.favouritePhrasesShift(phrase);
+      return "added";
 
     } catch (error) {
       console.log(error.message);
+      return "error";
     }
   }
 
