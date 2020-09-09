b8203e5c7db3a2c654381b9aaec77bb9b05a11d8
Eric Rovell <ericrovell@gmail.com>
2020-09-09 11:55:55 +0300

    using ts and notifications

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,28 @@
-<script>
-  import Router from "svelte-spa-router";
-  import routes from "@routes/routes.js";
+<script lang="ts">
+  import Cookie from "@app/Cookie";
+
+  import Router from "svelte-spa-router/Router.svelte";
+  import routes from "@routes/routes";
 
   import LayoutMain from "@src/layout/LayoutMain.svelte";
   import Modal from "@components/Modal.svelte";
+  import Notifications from "@components/notification/Notifications.svelte";
 </script>
 
-<svelte:head>
-  <title>Wise Cookie</title>
-</svelte:head>
+<!--
+  @component
+  Root component of the Application.
 
-<body>
-  <LayoutMain>
-    <Router {routes} />
-  </LayoutMain>
-  <Modal />
-</body>
+  Includes:
+    - Layout component with Routing Logic inside of it,
+    - Modal wrapper that gets content from the store.
+
+  Sets the global context of Cookie class instance with "cookie" key.
+  Cookie class provides all necessary methods to work with data,
+  specifically cookies and phrases.
+-->
+<LayoutMain>
+  <Router {routes} />
+</LayoutMain>
+<Modal />
+<Notifications />
