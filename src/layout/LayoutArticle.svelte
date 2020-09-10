4dfb3e8b3c99e06badcf1f210ee8dbf23997bfc3
Eric Rovell <ericrovell@gmail.com>
2020-09-10 14:45:53 +0300

    article common styles

--- src/layout/LayoutArticle.svelte
+++ src/layout/LayoutArticle.svelte
@@ -17,7 +17,38 @@
     font-size: 18px;
   }
 
-  .article p {
-    color: red;
+  :global(article h1) {
+    color: var(--color-3);
+    margin-top: 1em;
+    margin-bottom: 0.5em;
+    font-size: 1.4em;
+  }
+
+  :global(article h2) {
+    color: var(--color-3);
+    margin-top: 1em;
+    margin-bottom: 0.1em;
+    font-size: 1.1em;
+  }
+
+  :global(article p) {
+    text-indent: 0.75em;
+  }
+
+  :global(article ul) {
+    list-style: none;
+    counter-reset: options;
+  }
+
+  :global(article ul li) {
+    counter-increment: options
+  }
+
+  :global(ul li:before) {
+    content: counter(options) ". ";
+  }
+
+  :global(article > button) {
+    place-self: center;
   }
 </style>
