44b22d747d3815e501c9d04e3a2a577b2bd1fb85
Eric Rovell <ericrovell@gmail.com>
2020-09-20 01:00:52 +0300

    updated message

--- src/components/modals/Greetings.svelte
+++ src/components/modals/Greetings.svelte
@@ -7,11 +7,17 @@
 </script>
 
 <div class="modal">
-  <p>Welcome, dear user!</p>
-  <p>My name is MrCookie!</p>
-  <p>I can provide you cookies filled with a bit of wisdom.</p>
-  <p>Usually, it takes some time to make one, 8 hours to be specific!</p>
-  <p>Eat them to get your personal phrase.</p>
+  <p>Hello, dear friend!</p>
+  <p>
+    My name is MrCookie!
+    I am filled with a bit of wisdom. To get some, eat some cookies!
+  </p>
+  <p>
+    If you need some help, please come <a title="Help page" href="/#/help">here</a>!
+  </p>
+  <p>
+    And of course, we are privacy focused, read about it <a title="Help page" href="/#/help">here</a> and <a title="Privacy page" href="/#/privacy">here</a>.
+  </p>
   <div class="mr-cookie">
     <MrCookie />
   </div>
